e4fcd1a9c0513b16b6accc6eeed0512b61f4c81d
elya.tkd.itf <elya.tkd.itf@gmail.com>
2022-01-31 16:13:38 +0300

    add getting all info about knitting

--- serv.py
+++ serv.py
@@ -2,15 +2,26 @@ from flask import Flask, request
 import json
 import psycopg2
 
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 @app.route("/")
+@cross_origin()
 def hello():
     return "hello", 200
 
-@app.route("/knit_urls", methods=['GET'])
+@app.route("/knitting")
+@cross_origin()
 def get_knit_urls():
-    pass
+    cur.execute(f"select url, name, description from knitting")
+    rows = cur.fetchall()
+    all_info = list()
+    for row in rows:
+        all_info.append({"url":row[0], "name":row[1], "description":row[2]})
+    return json.dumps(all_info), 200
 
 if __name__ == "__main__":
     con = psycopg2.connect(
